8770e28ce2fc1437dce6e028bccab808b55db1c3
AvinashTiwari85 <qwe123kids@gmail.com>
2019-05-21 18:59:06 -0600

    no message

--- 4_WorkFlow/FontEnd/frontend/src/App.js
+++ 4_WorkFlow/FontEnd/frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-Hi there hello time        </p>
+Hi there hello time times        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
