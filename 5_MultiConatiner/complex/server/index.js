115d149dc8fad16689d3f18709dd5e252658d599
AvinashTiwari85 <qwe123kids@gmail.com>
2019-06-01 16:58:13 -0600

    no message

--- 5_MultiConatiner/complex/server/index.js
+++ 5_MultiConatiner/complex/server/index.js
@@ -21,3 +21,45 @@ pgClient.on('error', () => console.log('Connection error occured'));
 
 pgClient.query('CREATE TABLE IF NOT EXISTS values(NUMBER int)')
 .catch(err => console.log(err));
+
+const redis = require('redis')
+const redisClient = redis.createClient({
+    host: keys.redisHOST,
+    port: keys.redisPORT,
+    retry_strategy: () =>1000
+});
+
+const redishPublisher = redisClient.duplicate();
+
+app.get('/', (req,res) => {
+    res.send('Hi');
+});
+
+
+app.get('values/all', async(req,res) => {
+    const values = await pgClient.query('Select * from values');
+    res.send(values.row)
+});
+
+
+app.get('values/current', async(req,res) => {
+     redisClient.hgetall('values',(err,values) => {
+        res.send(values);
+     });
+});
+
+app.post('/values', async(req,res) => {
+    const index = req.body.index;
+    if(parseInt(index) > 40){
+        return res.status(442).send('value to high');
+    }
+
+    redisClient.hset('values', index,"Nothing Yet");
+    redishPublisher.publish('index', index);
+    pgClient.query('INSERT INTO VALUES(number) value($1)', [index]);
+    res.send({working: true});
+});
+
+app.listen(5000,err=>{
+    console.log("listening");
+})
