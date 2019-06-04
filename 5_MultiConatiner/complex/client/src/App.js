5594d58cdf3bc54b12c3fb9381215f453fab8533
AvinashTiwari85 <qwe123kids@gmail.com>
2019-06-03 19:43:24 -0600

    no message

--- 5_MultiConatiner/complex/client/src/App.js
+++ 5_MultiConatiner/complex/client/src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter as Router , Router, Link } from 'react-router-dom';
-import Otherpage from './Otherpage';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import OtherPage from './OtherPage';
 import Fib from './Fib';
 
-function App() {
-  return (
-    <Router>
-    <div className="App">
-      <header className="App-header">
-      <Link to="/"/>
-      <Link to="Otherpage">Other page</Link>
-       </header>
-      <div>
-        <Route exact path="/" Compotent={Fib}></Route>
-        <Route exact path="/Otherpage" Compotent={Otherpage}></Route>
-
-      </div>
-    </div>
-    </Router>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+            <Link to="/">Home</Link>
+            <Link to="/otherpage">Other Page</Link>
+          </header>
+          <div>
+            <Route exact path="/" component={Fib} />
+            <Route path="/otherpage" component={OtherPage} />
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
