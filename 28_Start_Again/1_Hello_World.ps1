9be2d5edb7462e16403316fb9214b9e421919c3b
AvinashTiwari85 <qwe123kids@gmail.com>
2020-07-13 16:22:24 -0600

    no message

---
+++ 28_Start_Again/1_Hello_World.ps1
@@ -0,0 +1,14 @@
+﻿docker container run --publish 80:80 nginx
+
+docker container run --publish 80:80 --detach nginx
+
+docker container ls
+
+docker container stop 2386
+
+docker container run --publish 80:80 --detach --name webhost nginx
+
+docker container logs webhost
+
+docker container --help
+
