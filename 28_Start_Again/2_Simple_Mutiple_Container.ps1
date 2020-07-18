175755547e03e50187ab788882db09251eb86497
AvinashTiwari85 <qwe123kids@gmail.com>
2020-07-17 18:57:02 -0600

    no message

---
+++ 28_Start_Again/2_Simple_Mutiple_Container.ps1
@@ -0,0 +1,9 @@
+﻿docker container run -d -p 3307:3306 --name db1 -e MYSQL_RANDOM_ROOT_PASSWORD=yes mysql
+#827d745a6330b4661286b15681e3e4ca8c634e630c1999302e21a4f4b2bf6d46
+
+docker container logs  db1
+
+docker container run -d  --name webserver -p 8080:80 httpd
+#b013b8338ef726e967f292688c682e7ac818b5e4eaef4a27b7c106d3e41c1e04
+
+docker ps
